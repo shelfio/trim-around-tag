3c7b8a6bd768aa4e4dacb1deed57abff14d1acff
Vlad Holubiev <golubev.vld@gmail.com>
2018-07-16 17:14:23 +0300

    feat: add trimTextAroundTag implementation

--- index.js
+++ index.js
@@ -1,11 +1,93 @@
-'use strict';
+const EM_OPEN_TAG = '<em>';
+const EM_CLOSE_TAG = '</em>';
 
-module.exports = (input, opts) => {
-  if (typeof input !== 'string') {
-    throw new TypeError(`Expected a string, got ${typeof input}`);
+module.exports.trimTextAroundTag = function({text = '', maxLengthAround = 200}) {
+  if (isTextTooSmallToTrim(text, maxLengthAround)) {
+    return text;
   }
 
-  opts = opts || {};
+  if (isTextWithoutHighlights(text)) {
+    return text;
+  }
 
-  return input + ' & ' + (opts.postfix || 'rainbows');
+  if (isTextAroundHighlightsSmallEnough({text, maxLengthAround})) {
+    return text;
+  }
+
+  const textBeforeHighlight = getTextBeforeHighlight(text);
+  const textAfterHighlight = getTextAfterHighlight(text);
+
+  return [
+    trimTextUntilSize(textBeforeHighlight, maxLengthAround),
+    getTextBetweenHighlightBoundaries(text),
+    trimTextUntilSizeFromEnd(textAfterHighlight, maxLengthAround)
+  ].join('');
 };
+
+function isTextTooSmallToTrim(text, maxLengthAround) {
+  return text.length <= maxLengthAround;
+}
+
+function isTextWithoutHighlights(text) {
+  const firstHighlightStartIndex = text.indexOf(EM_OPEN_TAG);
+
+  return firstHighlightStartIndex === -1;
+}
+
+function getTextBeforeHighlight(text) {
+  const firstHighlightStartIndex = text.indexOf(EM_OPEN_TAG);
+
+  return text.slice(0, firstHighlightStartIndex);
+}
+
+function getTextAfterHighlight(text) {
+  const lastHighlightEndIndex = text.lastIndexOf(EM_CLOSE_TAG) + EM_CLOSE_TAG.length;
+
+  return text.slice(lastHighlightEndIndex, text.length);
+}
+
+function isTextAroundHighlightsSmallEnough({text, maxLengthAround}) {
+  const textBeforeHighlight = getTextBeforeHighlight(text);
+  const textAfterHighlight = getTextAfterHighlight(text);
+  const isTextBeforeSmallEnough = textBeforeHighlight.length <= maxLengthAround;
+  const isTextAfterSmallEnough = textAfterHighlight.length <= maxLengthAround;
+
+  return isTextBeforeSmallEnough && isTextAfterSmallEnough;
+}
+
+function getTextBetweenHighlightBoundaries(text) {
+  const firstHighlightStartIndex = text.indexOf(EM_OPEN_TAG);
+  const lastHighlightEndIndex = text.lastIndexOf(EM_CLOSE_TAG) + EM_CLOSE_TAG.length;
+
+  return text.slice(firstHighlightStartIndex, lastHighlightEndIndex);
+}
+
+function trimTextUntilSize(text, maxLengthAround) {
+  const wordsInput = text.split(' ').reverse();
+  const wordsOutput = getWordsUntilLength(wordsInput, maxLengthAround);
+
+  return wordsOutput.reverse().join(' ');
+}
+
+function trimTextUntilSizeFromEnd(text, maxLengthAround) {
+  const wordsInput = text.split(' ');
+  const wordsOutput = getWordsUntilLength(wordsInput, maxLengthAround);
+
+  return wordsOutput.join(' ');
+}
+
+function getWordsUntilLength(words, maxLength) {
+  const wordsOutput = [];
+  let outputLength = 0;
+
+  for (let word of words) {
+    const isTextStillShort = outputLength <= maxLength;
+
+    if (isTextStillShort) {
+      wordsOutput.push(word);
+      outputLength += word.length + 1;
+    }
+  }
+
+  return wordsOutput;
+}
--- index.test.js
+++ index.test.js
@@ -1,5 +1,124 @@
-const m = require('.');
+const {trimTextAroundTag} = require('./index');
 
-it('should export a module', () => {
-  expect(m).toBeInstanceOf(Function);
+describe('#trimTextAroundTag', () => {
+  it('should export trimTextAroundTag', () => {
+    expect(trimTextAroundTag).toBeInstanceOf(Function);
+  });
+
+  it('should return text as it is when not so many chars aruond highlight', () => {
+    const text = ' <em>strongest</em>  cyclone in the Arabian Sea.';
+    expect(trimTextAroundTag({text})).toEqual(text);
+  });
+
+  it('should truncate text to ~200 chars around highlight (sample 1)', () => {
+    const text = [
+      `The storm was rapidly intensifying at the time, after the wind shear `,
+      `had decreased,[2] and the eye contracted to a diameter of 19 km (12 mi).`,
+      `[10] At 09:00 UTC on October 28, the IMD upgraded Nilofar further to an `,
+      `extremely severe cyclonic storm.[2][nb 1] Six hours later, the JTWC `,
+      `estimated peak 1-minute winds of 215 km/h (130 mph);[5] at the time, the `,
+      `agency anticipated further strengthening due to the favorable conditions and good `,
+      `organization.[12] At 18:00 UTC on October 28, the IMD estimated peak 3-minute winds of `,
+      `205 km/h (125 mph).[2] At the time, it was the <em>third</em>-<em>strongest</em> storm on `,
+      `record in the Arabian Sea.[13] On October 29, Nilofar started weakening due to increased wind `,
+      `shear, and the convection diminished in intensity.[14] At the same time, the storm turned `,
+      `northeastward while rounding the ridge to the east.[15] Increasingly cooler and drier air, `,
+      `as well as cooler waters, caused the storm to degrade rapidly.[2] The eye, previously small `,
+      `and well defined, dissipated by 06:00 UTC on October 29.[16] Lateras well as cooler waters, `,
+      `caused the storm to degrade rapidly.[2] The eye, previously small and well defined, dissipated by 06:00 UTC on October 29.[16] Later`
+    ].join('');
+    const result = trimTextAroundTag({text});
+
+    expect(result).toEqual(
+      [
+        `anticipated further strengthening due to the favorable conditions and good organization.`,
+        `[12] At 18:00 UTC on October 28, the IMD estimated peak 3-minute winds of 205 km/h (125 mph).`,
+        `[2] At the time, it was the <em>third</em>-<em>strongest</em> storm on record in the Arabian `,
+        `Sea.[13] On October 29, Nilofar started weakening due to increased wind shear, and the convection `,
+        `diminished in intensity.[14] At the same time, the storm turned northeastward`
+      ].join('')
+    );
+  });
+
+  it('should truncate text to ~200 chars around highlight (sample 2)', () => {
+    // lots of text between 2 highlights
+    const text = [
+      `Extremely Severe Cyclonic Storm Nilofar Extremely severe cyclonic storm (IMD scale) `,
+      `Category 4 (Saffir–Simpson scale) Nilofar shortly before peak intensity on October 28, `,
+      `2014 Formed October 25, 2014 Dissipated October 31, 2014 Highest winds 3-minute sustained: `,
+      `205 km/h (125 mph) 1-minute sustained: 215 km/h (130 mph) Lowest pressure 950 hPa (mbar); `,
+      `28.05 inHg Fatalities 4 total Damage Minimal Areas affected Oman, India, Pakistan Part of `,
+      `the 2014 North Indian Ocean cyclone season Cyclone Nilofar Extremely Severe Cyclonic Storm `,
+      `Nilofar was, at the time, the <em>third</em>- <em>strongest</em> cyclone in the Arabian Sea. `,
+      `In late October 2014, it reached peak maximum sustained winds estimated between 205 km/h `,
+      `(125 mph) and 215 km/h (130 mph). The India Meteorological Department (IMD) named it Nilofar; `,
+      `the name refers to the water lily, and was suggested by Pakistan.[1] The western fringes of `,
+      `the storm caused flash flooding in northeastern Oman, killing four people.Category 4 `,
+      `(Saffir–Simpson scale) Nilofar shortly before peak intensity on October 28, 2014 `,
+      `Formed October 25, 2014 Dissipated October 31, 2014 Highest winds 3-minute sustained: `,
+      `205 km/h (125 mph) 1-minute sustained: 215 km/h (130 mph) Lowest pressure 950 hPa (mbar); `,
+      `28.05 inHg Fatalities 4 total Damage Minimal Areas affected Oman, India, Pakistan Part of `,
+      `the 2014 North Indian Ocean cyclone season Cyclone Nilofar Extremely Severe Cyclonic Storm `,
+      `Nilofar was, at the time, the <em>third</em>- <em>strongest</em> cyclone in the Arabian Sea. `,
+      `In late October 2014, it reached peak maximum sustained winds estimated between 205 km/h `,
+      `(125 mph) and 215 km/h (130 mph). The India Meteorological Department (IMD) named it Nilofar; `,
+      `the name refers to the water lily, and was suggested by Pakistan.[1] `,
+      `The western fringes of the storm caused flash flooding in northeastern Oman, killing four people.`
+    ].join('');
+    const result = trimTextAroundTag({text});
+
+    expect(result).toEqual(
+      [
+        `Fatalities 4 total Damage Minimal Areas affected Oman, India, Pakistan Part of the 2014 North Indian Ocean `,
+        `cyclone season Cyclone Nilofar Extremely Severe Cyclonic Storm Nilofar was, at the time, the <em>third</em>- <em>strongest</em> cyclone `,
+        `in the Arabian Sea. In late October 2014, it reached peak maximum sustained winds `,
+        `estimated between 205 km/h (125 mph) and 215 km/h (130 mph). The India Meteorological Department (IMD) named it Nilofar; `,
+        `the name refers to the water lily, and was suggested by Pakistan.[1] The western fringes of the storm caused flash flooding in northeastern Oman, `,
+        `killing four people.Category 4 (Saffir–Simpson scale) Nilofar shortly before peak intensity on October `,
+        `28, 2014 Formed October 25, 2014 Dissipated October 31, 2014 Highest winds 3-minute sustained: `,
+        `205 km/h (125 mph) 1-minute sustained: 215 km/h (130 mph) Lowest pressure 950 hPa (mbar); 28.05 inHg Fatalities `,
+        `4 total Damage Minimal Areas affected Oman, India, Pakistan Part of the 2014 North Indian Ocean cyclone `,
+        `season Cyclone Nilofar Extremely Severe Cyclonic Storm Nilofar was, at the time, the <em>third</em>- <em>strongest</em> cyclone `,
+        `in the Arabian Sea. In late October 2014, it reached peak maximum sustained winds estimated between 205 km/h (125 mph) and 215 km/h (130 mph). The India Meteorological Department (IMD) named it`
+      ].join('')
+    );
+  });
+
+  it('should truncate text to ~50 chars around highlight', () => {
+    const text = [
+      `The storm was rapidly intensifying at the time, after the wind shear `,
+      `had decreased,[2] and the eye contracted to a diameter of 19 km (12 mi).`,
+      `[10] At 09:00 UTC on October 28, the IMD upgraded Nilofar further to an `,
+      `extremely severe cyclonic storm.[2][nb 1] Six hours later, the JTWC `,
+      `estimated peak 1-minute winds of 215 km/h (130 mph);[5] at the time, the `,
+      `agency anticipated further strengthening due to the favorable conditions and good `,
+      `organization.[12] At 18:00 UTC on October 28, the IMD estimated peak 3-minute winds of `,
+      `205 km/h (125 mph).[2] At the time, it was the <em>third</em>-<em>strongest</em> storm on `,
+      `record in the Arabian Sea.[13] On October 29, Nilofar started weakening due to increased wind `,
+      `shear, and the convection diminished in intensity.[14] At the same time, the storm turned `,
+      `northeastward while rounding the ridge to the east.[15] Increasingly cooler and drier air, `,
+      `as well as cooler waters, caused the storm to degrade rapidly.[2] The eye, previously small `,
+      `and well defined, dissipated by 06:00 UTC on October 29.[16] Lateras well as cooler waters, `,
+      `caused the storm to degrade rapidly.[2] The eye, previously small and well defined, dissipated by 06:00 UTC on October 29.[16] Later`
+    ].join('');
+    const result = trimTextAroundTag({text, maxLengthAround: 50});
+
+    expect(result).toEqual(
+      `of 205 km/h (125 mph).[2] At the time, it was the <em>third</em>-<em>strongest</em> storm on record in the Arabian Sea.[13] On October`
+    );
+  });
+
+  it('should not truncate text which has no highlights', () => {
+    const text = `The storm was rapidly intensifying at the time, after the wind shear`;
+    const result = trimTextAroundTag({text, maxLengthAround: 50});
+
+    expect(result).toEqual(text);
+  });
+
+  it('should not truncate text which has only highlights', () => {
+    const text = `<em>hello</em>`;
+    const result = trimTextAroundTag({text, maxLengthAround: 10});
+
+    expect(result).toEqual(text);
+  });
 });
